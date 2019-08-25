159521c5bac21ac9f8011987b08fb8af55ab1e29
yeajunseok <yeajunseok@gmail.com>
2019-08-25 21:25:49 +0900

    blog post list (index)

--- blog/models.py
+++ blog/models.py
@@ -9,5 +9,5 @@ class Post(models.Model):
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True) #on_delete는 사용자가 퇄퇴했을때 글등이 같이 삭제된다.
 
-    def __str__(self): #post 목록 이름이 어떻게 출력되는지 설정
+    def __str__(self): #post 목록 이 름이 어떻게 출력되는지 설정
         return '{} :: {}'.format(self.title, self.author)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    return render(
+        request,
+        'blog/index.html',
+        {
+            # 'posts'랑 'a_plus_b' 가 templates/blog/index.html 파일의 {{ '제목' }} 으로 들어간다.
+            'posts': posts,
+            'a_plus_b': 1+3,
+        }
+    )
